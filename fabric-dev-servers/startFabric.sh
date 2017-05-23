3227a915b8f29a280b803475af75cb57121dc3a9
Matthew B White <whitemat@uk.ibm.com>
2017-05-23 10:56:30 +0100

    counchdb

--- fabric-dev-servers/startFabric.sh
+++ fabric-dev-servers/startFabric.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd "$( dirname "${BASH_SOURCE[0]}" )" && npm install 
 
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/_loader.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+npm install --prefix "${DIR}"
+
+source "${DIR}"/_loader.sh
