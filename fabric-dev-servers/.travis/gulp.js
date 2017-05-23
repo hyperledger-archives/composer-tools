5f2d7c5f020ed361107079d185394b5b340445bf
Matthew B White <whitemat@uk.ibm.com>
2017-05-23 13:46:30 +0100

    add in package.json

--- fabric-dev-servers/.travis/gulp.js
+++ fabric-dev-servers/.travis/gulp.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 
 gulp.task('default', () =>
-    gulp.src(['../*.sh','../fabric-scripts/**/*'  ], { base : '..'})
+    gulp.src(['../package.json','../*.sh','../fabric-scripts/**/*'  ], { base : '..'})
         .pipe(zip('fabric-dev-servers.zip'))
         .pipe(gulp.dest('..'))
 );
