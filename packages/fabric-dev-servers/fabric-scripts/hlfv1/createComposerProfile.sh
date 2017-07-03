114bd3a9cf4d4d96f43f78aa27d001041c5c5b2c
Dave Kelsey <d_kelsey@uk.ibm.com>
2017-07-03 11:29:24 +0100

    upgrade to rc1
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/fabric-dev-servers/fabric-scripts/hlfv1/createComposerProfile.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv1/createComposerProfile.sh
@@ -8,7 +8,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf ~/.composer-connection-profiles/hlfv1/*
 rm -rf ~/.hfc-key-store/*
 
-# copy peer admin credentials into the keyValStore
+# copy org admin credentials into the keyValStore
 mkdir -p ~/.hfc-key-store
 cp "${DIR}"/composer/creds/* ~/.hfc-key-store
 
@@ -20,15 +20,17 @@ cat << EOF > ~/.composer-connection-profiles/hlfv1/connection.json
     "orderers": [
        { "url" : "grpc://localhost:7050" }
     ],
-    "ca": { "url": "http://localhost:7054", "name": "ca.example.com"},
+    "ca": { "url": "http://localhost:7054", 
+            "name": "ca.org1.example.com"
+    },
     "peers": [
         {
             "requestURL": "grpc://localhost:7051",
             "eventURL": "grpc://localhost:7053"
         }
     ],
-    "keyValStore": "${HOME}/.hfc-key-store",
-    "channel": "mychannel",
+    "keyValStore": "${HOME}/.composer-wallet",
+    "channel": "composerchannel",
     "mspID": "Org1MSP",
     "timeout": "300"
 }
