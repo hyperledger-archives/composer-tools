267118b7b4772d52427bbf25db4183bd40bf457e
Matthew B White <whitemat@uk.ibm.com>
2017-11-06 12:43:55 +0000

    updated the peer admin card connection profile, and explicitly added in the certificate
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Exit on first error, print all commands.
-# set -ev
+# Exit on first error
+set -e
 # Grab the current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo
 # check that the composer command exists at a version >v0.14
 if hash composer 2>/dev/null; then
-    composer --version | awk -F. '{if ($2<14) exit 1}'
+    composer --version | awk -F. '{if ($2<15) exit 1}'
     if [ $? -eq 1 ]; then
-        echo 'Sorry, Need to have composer-cli installed at v0.15 or greater' 
+        echo 'Sorry, Use createConnectionProfile for versions before v0.15.0' 
         exit 1
     else
         echo Using composer-cli at $(composer --version)
@@ -21,7 +21,6 @@ else
 fi
 # need to get the certificate 
 
-cat "${DIR}"/composer/creds/PeerAdmin | jq -r .enrollment.identity.certificate > /tmp/.PeerAdmin.cert 
 cat << EOF > /tmp/.connection.json
 {
     "name": "hlfv1",
@@ -29,26 +28,25 @@ cat << EOF > /tmp/.connection.json
     "orderers": [
        { "url" : "grpc://localhost:7050" }
     ],
-    "ca": { "url": "http://localhost:7054", "name": "ca.example.com"},
+    "ca": { "url": "http://localhost:7054", "name": "ca.org1.example.com"},
     "peers": [
         {
             "requestURL": "grpc://localhost:7051",
             "eventURL": "grpc://localhost:7053"
         }
     ],
-    "keyValStore": "${HOME}/.hfc-key-store",
     "channel": "composerchannel",
     "mspID": "Org1MSP",
-    "timeout": "300"
+    "timeout": 300
 }
 EOF
 
-PRIVATE_KEY="${DIR}"/composer/creds/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457-priv
+PRIVATE_KEY="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
+CERT="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
 
-composer card create -j /tmp/.connection.json -u PeerAdmin -c /tmp/.PeerAdmin.cert -k "${PRIVATE_KEY}" -r PeerAdmin --file /tmp/PeerAdmin@hlfv1.card 
+composer card create -j /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin --file /tmp/PeerAdmin@hlfv1.card 
 composer card import --file /tmp/PeerAdmin@hlfv1.card 
 
-rm -rf /tmp/.PeerAdmin.cert 
 rm -rf /tmp/.connection.json
 
 echo "Hyperledger Composer PeerAdmin card has been imported"
