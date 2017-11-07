e34862cf087a4f12e1ab78d251ceefacad093df7
Matthew B White <whitemat@uk.ibm.com>
2017-11-07 16:19:43 +0000

    correct the command line option on the createPeerAdminCard.sh
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -44,7 +44,7 @@ EOF
 PRIVATE_KEY="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
 CERT="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
 
-composer card create -j /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin --file /tmp/PeerAdmin@hlfv1.card 
+composer card create -p /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin --file /tmp/PeerAdmin@hlfv1.card 
 composer card import --file /tmp/PeerAdmin@hlfv1.card 
 
 rm -rf /tmp/.connection.json
