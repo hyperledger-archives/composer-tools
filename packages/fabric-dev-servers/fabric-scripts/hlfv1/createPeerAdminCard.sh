9c731e685a91767263dc03c6f80d928ce9b0f6f9
Simon Stone <sstone1@uk.ibm.com>
2017-11-08 21:21:50 +0000

    Delete existing business network card if required (#68)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -45,6 +45,9 @@ EOF
 PRIVATE_KEY="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
 CERT="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
 
+if composer card list -n PeerAdmin@hlfv1 > /dev/null; then
+    composer card delete -n PeerAdmin@hlfv1
+fi
 composer card create -p /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin --file /tmp/PeerAdmin@hlfv1.card 
 composer card import --file /tmp/PeerAdmin@hlfv1.card 
 
