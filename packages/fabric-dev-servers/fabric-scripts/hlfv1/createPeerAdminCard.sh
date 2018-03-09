2549ee723792d38292390b9d57e43ed676c28da8
Ellis Henderson <ellis.44@hotmail.co.uk>
2018-03-09 09:15:59 +0000

    Changes to the hlfv1 to use composer-cli (#106)
    Signed-off-by: Ellis Henderson <ellis.henderson@ibm.com>

--- packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv1/createPeerAdminCard.sh
@@ -5,15 +5,19 @@ set -e
 # Grab the current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ -z "${HL_COMPOSER_CLI}" ]; then
+  HL_COMPOSER_CLI=$(which composer)
+fi
+
 echo
 # check that the composer command exists at a version >v0.14
-if hash composer 2>/dev/null; then
-    composer --version | awk -F. '{if ($2<15) exit 1}'
+if hash "${HL_COMPOSER_CLI}" 2>/dev/null; then
+    "${HL_COMPOSER_CLI}" --version | awk -F. '{if ($2<15) exit 1}'
     if [ $? -eq 1 ]; then
         echo 'Sorry, Use createConnectionProfile for versions before v0.15.0' 
         exit 1
     else
-        echo Using composer-cli at $(composer --version)
+        echo Using composer-cli at $("${HL_COMPOSER_CLI}" --version)
     fi
 else
     echo 'Need to have composer-cli installed at v0.15 or greater'
@@ -44,14 +48,14 @@ EOF
 PRIVATE_KEY="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
 CERT="${DIR}"/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
 
-if composer card list -n PeerAdmin@hlfv1 > /dev/null; then
-    composer card delete -n PeerAdmin@hlfv1
+if "${HL_COMPOSER_CLI}" card list -n PeerAdmin@hlfv1 > /dev/null; then
+    "${HL_COMPOSER_CLI}" card delete -n PeerAdmin@hlfv1
 fi
-composer card create -p /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin -r ChannelAdmin --file /tmp/PeerAdmin@hlfv1.card
-composer card import --file /tmp/PeerAdmin@hlfv1.card 
+"${HL_COMPOSER_CLI}" card create -p /tmp/.connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin -r ChannelAdmin --file /tmp/PeerAdmin@hlfv1.card
+"${HL_COMPOSER_CLI}" card import --file /tmp/PeerAdmin@hlfv1.card 
 
 rm -rf /tmp/.connection.json
 
 echo "Hyperledger Composer PeerAdmin card has been imported"
-composer card list
+"${HL_COMPOSER_CLI}" card list
 
